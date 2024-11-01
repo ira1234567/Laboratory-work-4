e289e1ccca78b8a57dafb8d6d3af242223ebd270
ira1234567 <127392659+ira1234567@users.noreply.github.com>
2024-11-01 18:35:36 +0200

    Update dataschemamodified.sql

--- dataschemamodified.sql
+++ dataschemamodified.sql
@@ -1,12 +1,18 @@
 CREATE TABLE koristuvach (
     id INT PRIMARY KEY,
-    imya VARCHAR(100) CHECK (REGEXP_LIKE(imya, '^[A-Za-zА-Яа-яЁёІіЇїЄєҐґ\s]{1,100}$')),
-    rol VARCHAR(50) CHECK (REGEXP_LIKE(rol, '^[A-Za-zА-Яа-яЁёІіЇїЄєҐґ\s]{1,50}$'))
+    imya VARCHAR(100) CHECK (
+        REGEXP_LIKE(imya, '^[A-Za-zА-Яа-яЁёІіЇїЄєҐґ\s]{1,100}$')
+    ),
+    rol VARCHAR(50) CHECK (
+        REGEXP_LIKE(rol, '^[A-Za-zА-Яа-яЁёІіЇїЄєҐґ\s]{1,50}$')
+    )
 );
 
 CREATE TABLE pysmennyk (
     id INT PRIMARY KEY,
-    CONSTRAINT pysmennyk_koristuvach_fk FOREIGN KEY (id) REFERENCES koristuvach(id)
+    CONSTRAINT pysmennyk_koristuvach_fk FOREIGN KEY (
+        id
+    ) REFERENCES koristuvach (id)
 );
 
 CREATE TABLE riven_osvitlenosti (
@@ -16,7 +22,7 @@ CREATE TABLE riven_osvitlenosti (
     time_column TIME,
     istoriya_osvitlenosti_id INT,
     CONSTRAINT riven_osvitlenosti_istoriya_fk
-        FOREIGN KEY (istoriya_osvitlenosti_id) REFERENCES istoriya_osvitlenosti(id)
+    FOREIGN KEY (istoriya_osvitlenosti_id) REFERENCES istoriya_osvitlenosti (id)
 );
 
 CREATE TABLE istoriya_osvitlenosti (
@@ -24,7 +30,7 @@ CREATE TABLE istoriya_osvitlenosti (
     pysmennyk_id INT,
     zmina_rivnya_osvitlenosti FLOAT,
     CONSTRAINT istoriya_osvitlenosti_pysmennyk_fk
-        FOREIGN KEY (pysmennyk_id) REFERENCES pysmennyk(id)
+    FOREIGN KEY (pysmennyk_id) REFERENCES pysmennyk (id)
 );
 
 CREATE TABLE tvir (
@@ -38,7 +44,7 @@ CREATE TABLE rekomendatsii (
     id INT PRIMARY KEY,
     tekst_rekomendatsii TEXT,
     tvir_id INT,
-    CONSTRAINT rekomendatsii_tvir_fk FOREIGN KEY (tvir_id) REFERENCES tvir(id)
+    CONSTRAINT rekomendatsii_tvir_fk FOREIGN KEY (tvir_id) REFERENCES tvir (id)
 );
 
 CREATE TABLE chernetka (
@@ -46,6 +52,8 @@ CREATE TABLE chernetka (
     nazva_chernetky VARCHAR(200) CHECK (LENGTH(nazva_chernetky) <= 200),
     pysmennyk_id INT,
     tvir_id INT,
-    CONSTRAINT chernetka_pysmennyk_fk FOREIGN KEY (pysmennyk_id) REFERENCES pysmennyk(id),
-    CONSTRAINT chernetka_tvir_fk FOREIGN KEY (tvir_id) REFERENCES tvir(id)
+    CONSTRAINT chernetka_pysmennyk_fk FOREIGN KEY (
+        pysmennyk_id
+    ) REFERENCES pysmennyk (id),
+    CONSTRAINT chernetka_tvir_fk FOREIGN KEY (tvir_id) REFERENCES tvir (id)
 );
